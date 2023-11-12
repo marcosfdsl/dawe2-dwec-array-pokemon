fcfde514bc792391ba15382f5ba78f8537556e6c
marcosfdsl <marcos.fernandez@live.u-tad.com>
2023-11-12 17:19:06 +0100

    cb1

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
 
+export class HeaderComponent implements AfterViewInit {
+  private botonPokeball: HTMLElement | null = null;
+  private rojoPokeball: HTMLElement | null = null;
+
+  ngAfterViewInit() {
+    this.botonPokeball = document.querySelector('.botonpokeball');
+    this.rojoPokeball = document.querySelector('.rojopokeball');
+
+    if (this.botonPokeball && this.rojoPokeball) {
+      this.botonPokeball.addEventListener('mouseenter', () => this.onMouseEnter());
+      this.botonPokeball.addEventListener('mouseleave', () => this.onMouseLeave());
+    }
+  }
+
+  onMouseEnter() {
+    if (this.rojoPokeball) {
+      this.rojoPokeball.style.opacity = '1';
+      this.rojoPokeball.style.visibility = 'visible';
+    }
+  }
+
+  onMouseLeave() {
+    if (this.rojoPokeball) {
+      this.rojoPokeball.style.opacity = '0';
+      this.rojoPokeball.style.visibility = 'hidden';
+    }
+  }
+  
 }
--- src/app/pokemons/pokemons.component.ts
+++ src/app/pokemons/pokemons.component.ts
@@ -13,6 +13,49 @@ export class PokemonsComponent {
     { id: 4, foto: 'assets/fotos/charmander.png', nombre: 'Charmander', codigo: '004', tipo: 'Fuego', altura: 0.60, peso: 8.50 },
     { id: 5, foto: 'assets/fotos/charmeleon.png', nombre: 'Charmeleon', codigo: '005', tipo: 'Fuego', altura: 1.10, peso: 19.00 },
     { id: 6, foto: 'assets/fotos/charizard.png', nombre: 'Charizard', codigo: '006', tipo: 'Fuego, Volador', altura: 1.70, peso: 90.50 },
-    // Otros objetos Pokemon
   ];
+
+  private botonPokeball: HTMLElement | null = null;
+  private rojoPokeball: HTMLElement | null = null;
+  private brightnessFilter: boolean = false;
+
+  ngAfterViewInit() {
+    this.botonPokeball = document.querySelector('.botonpokeball');
+    this.rojoPokeball = document.querySelector('.rojopokeball');
+
+    if (this.botonPokeball && this.rojoPokeball) {
+      this.botonPokeball.addEventListener('mouseenter', () => this.onMouseEnter());
+      this.botonPokeball.addEventListener('mouseleave', () => this.onMouseLeave());
+      this.botonPokeball.addEventListener('click', () => this.toggleFotosFilter());
+    }
+  }
+
+  onMouseEnter() {
+    if (this.rojoPokeball) {
+      this.rojoPokeball.style.opacity = '1';
+      this.rojoPokeball.style.visibility = 'visible';
+    }
+  }
+
+  onMouseLeave() {
+    if (this.rojoPokeball) {
+      this.rojoPokeball.style.opacity = '0';
+      this.rojoPokeball.style.visibility = 'hidden';
+    }
+  }
+
+  toggleFotosFilter() {
+    const fotosElements = document.querySelectorAll('.fotos');
+  
+    if (fotosElements) {
+      fotosElements.forEach((element: Element) => {
+        const htmlElement = element as HTMLElement;
+        htmlElement.style.filter = this.brightnessFilter ? 'brightness(100%)' : 'brightness(0%)';
+      });
+  
+      this.brightnessFilter = !this.brightnessFilter;
+    }
+  }
+  
+  
 }
